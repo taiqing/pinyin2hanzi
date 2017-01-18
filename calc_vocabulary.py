f275120743ebc87b064a536351645fc2d1f84d56
Wang Taiqing <taiqingwang@creditease.cn>
2017-01-18 18:02:36 +0800

    setup_corpus: setup_corpus.py: dump vocabulary

--- calc_vocabulary.py
+++ calc_vocabulary.py
@@ -1,8 +1,19 @@
 # coding=utf-8
 
+import cPickle
+
+def make_vocabulary(vocab):
+    vocab2 = dict()
+    i = 0
+    for k in vocab:
+        vocab2[k] = i
+        i += 1
+    return vocab2
+    
 
 if __name__ == '__main__':
     labeled_corpus_path = 'dataset/labeled_corpus_no_rare.txt'
+    vocab_file = 'dataset/vocab.pkl'
 
     vocab_source = dict()
     vocab_target = dict()
@@ -10,14 +21,14 @@ if __name__ == '__main__':
         for line in f:
             line = line.decode('utf-8')
             if line.startswith('P:'):
-                for uchar in line:
+                for uchar in line[2:]:
                     if uchar != u'\n':
                         if uchar not in vocab_source:
                             vocab_source[uchar] = 1
                         else:
                             vocab_source[uchar] += 1
             if line.startswith('Z:'):
-                for uchar in line:
+                for uchar in line[2:]:
                     if uchar != u'\n':
                         if uchar not in vocab_target:
                             vocab_target[uchar] = 1
@@ -25,3 +36,7 @@ if __name__ == '__main__':
                             vocab_target[uchar] += 1
     print 'source vocabulary size is {}'.format(len(vocab_source))
     print 'target vocabulary size is {}'.format(len(vocab_target))
+    
+    vocab_source = make_vocabulary(vocab_source)
+    vocab_target = make_vocabulary(vocab_target)
+    cPickle.dump((vocab_source, vocab_target), open(vocab_file, 'wb'))
